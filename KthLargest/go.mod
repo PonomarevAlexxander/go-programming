2367db38c90a8a76219a2aea2b471c1de2924474
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-10-27 22:50:52 +0300

    Add simple impementation of KthLargest

---
+++ KthLargest/go.mod
@@ -0,0 +1,3 @@
+module go/course/kth_largest
+
+go 1.21.3
--- KthLargest/main.go
+++ KthLargest/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 func findKthLargest(nums []int, k int) int {
 	sort.Slice(nums, func(i, j int) bool {
-		return i > j
+		return nums[i] > nums[j]
 	})
+	fmt.Println(nums)
 	return nums[k-1]
 }
