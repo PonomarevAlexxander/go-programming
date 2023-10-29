df9757a42529dafda2bf6439ccf184e863342ed8
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-10-29 23:03:04 +0300

    Solve task and add support for several modules

--- KthLargest/main.go
+++ KthLargest/main.go
@@ -1,14 +1,29 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
 func findKthLargest(nums []int, k int) int {
-	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] > nums[j]
-	})
-	fmt.Println(nums)
-	return nums[k-1]
+	return findKthSmallest(nums, len(nums)-k+1)
+}
+
+func findKthSmallest(nums []int, k int) int {
+	pivotIndex := partition(nums)
+	if pivotIndex+1 == k {
+		return nums[pivotIndex]
+	}
+	if pivotIndex+1 < k {
+		return findKthSmallest(nums[pivotIndex+1:], k-pivotIndex-1)
+	}
+	return findKthSmallest(nums[:pivotIndex], k)
+}
+
+func partition(nums []int) int {
+	pivot := nums[len(nums)-1]
+	pivotIndex := 0
+	for index, element := range nums {
+		if element < pivot {
+			nums[pivotIndex], nums[index] = nums[index], nums[pivotIndex]
+			pivotIndex++
+		}
+	}
+	nums[pivotIndex], nums[len(nums)-1] = nums[len(nums)-1], nums[pivotIndex]
+	return pivotIndex
 }
