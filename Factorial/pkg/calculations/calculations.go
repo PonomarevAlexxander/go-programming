853c8ec2e1cb9afea4052369dbdc44a18cf89a78
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-11-06 00:52:03 +0300

    Add calculations package

---
+++ Factorial/pkg/calculations/calculations.go
@@ -0,0 +1,32 @@
+package calculations
+
+import (
+	"errors"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func Calculate(number int64, logging bool) (int64, error) {
+	if number < 0 {
+		return 0, errors.New("number should be not negative")
+	}
+
+	if logging {
+		factorialLog := log.WithFields(log.Fields{
+			"number":  number,
+			"logging": logging,
+		})
+		factorialLog.Info("Start calculations...")
+		factorialLog.Infof("Calculate %d!", number)
+		defer factorialLog.Info("Calculations complete!")
+	}
+
+	return factorial(number), nil
+}
+
+func factorial(number int64) int64 {
+	if number == 0 {
+		return 1
+	}
+	return number * factorial(number-1)
+}
---
+++ Factorial/pkg/calculations/calculations_test.go
@@ -0,0 +1,32 @@
+package calculations
+
+import "testing"
+
+func TestCalculate(t *testing.T) {
+	type args struct {
+		number  int64
+		logging bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int64
+		wantErr bool
+	}{
+		{"valid data test 1", args{12, false}, 479001600, false},
+		{"valid data test 2", args{0, false}, 1, false},
+		{"invalid data test 1", args{-1, false}, 0, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Calculate(tt.args.number, tt.args.logging)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Calculate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
