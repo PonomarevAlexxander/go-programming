f31908dc7610a849e8bf0f5467c246c0b3f5d428
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-11-06 00:53:23 +0300

    Init new Factorial module

---
+++ Factorial/cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"go/course/factorial/pkg/calculations"
+	"os"
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	var loggingFlag = flag.Bool("log", false, "if flag is set, you will see logging msgs from inner functions")
+	flag.Parse()
+
+	log.SetLevel(log.InfoLevel)
+	log.SetOutput(os.Stdout)
+
+	if len(os.Args) < 2 {
+		log.Fatal("args should contain minimum 1 parameter")
+	}
+	number, err := strconv.ParseInt(flag.Arg(0), 10, 64)
+	if err != nil {
+		log.Fatal("args should contain number for factorial calculation")
+	}
+
+	result, err := calculations.Calculate(number, *loggingFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Factorial of %d is %d", number, result)
+}
