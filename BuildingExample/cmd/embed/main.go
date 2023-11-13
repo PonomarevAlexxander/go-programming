20d0f5709557f9f2759d9fbf5caee4ae61fb4c7d
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-11-13 19:24:37 +0300

    Add more examples for embed

--- BuildingExample/cmd/embed/main.go
+++ BuildingExample/cmd/embed/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	_ "embed"
+	"embed"
 )
 
 //go:embed text.txt
 var fileString string
 
+//go:embed text.txt
+var fileByte []byte
+
+//go:embed testfiles/*.hash
+var folder embed.FS
+
 func main() {
 	println(fileString)
+	println(string(fileByte))
+
+	file1, _ := folder.ReadFile("testfiles/file1.hash")
+	println(string(file1))
+	file2, _ := folder.ReadFile("testfiles/file2.hash")
+	println(string(file2))
 }
